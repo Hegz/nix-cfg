8675a707e7bde5566ef37fa82a10b98527e2eafb
Adam Fairbrother <adam.fairbrother@gmail.com>
2023-09-04 23:08:22 -0700

    Adjusted tmux Screen commands

--- home.nix
+++ home.nix
@@ -73,12 +73,23 @@
       userEmail = "adam.fairbrother@gmail.com";
     };
     tmux ={
+      baseIndex = 1;
       customPaneNavigationAndResize = true;
       enable = true;
       keyMode = "vi";
       mouse = false;
       prefix = "C-a";
       sensibleOnTop = true; 
+      extraConfig = ''
+        # C-a C-a swap to last window
+        bind-key C-a last-window
+
+        # C-a a send C-a
+        bind-key a send-prefix
+
+        # Auto rename windows
+        set-window-option -g automatic-rename
+      '';
 
     };
     ssh ={
