5ff0d21ffa529420a227a276cfd0bb98eb5a2a28
Adam Fairbrother <adam.fairbrother@gmail.com>
2023-09-19 14:33:28 -0700

    Added quickemu and sorted packages

--- Cromulent.nix
+++ Cromulent.nix
@@ -23,28 +23,29 @@
     description = "Adam Fairbrother";
     extraGroups = [ "networkmanager" "wheel" "distrobox" "docker"];
     packages = with pkgs; [
-      firefox
       chromium
-      teams
-      tigervnc
-      libsForQt5.bluedevil
-      libsForQt5.kalk
-      libsForQt5.yakuake
-      libsForQt5.okular
-      libsForQt5.ark
-      libsForQt5.gwenview
-      x2goclient
-      logseq
+      firefox
       freecad
-      libreoffice-fresh
       gimp-with-plugins
-      inkscape-with-extensions
-      tenacity
-      playonlinux
       git
-      kate
-      xclip
       google-chrome
+      inkscape-with-extensions
+      kate
+      libreoffice-fresh
+      libsForQt5.ark
+      libsForQt5.bluedevil
+      libsForQt5.gwenview
+      libsForQt5.kalk
+      libsForQt5.okular
+      libsForQt5.yakuake
+      logseq
+      playonlinux
+      quickemu
+      teams
+      tenacity
+      tigervnc
+      x2goclient
+      xclip
     ];
   };
 }
