a6228de4c5a14e7cab7bc4d0656852e8ba0d6b76
Adam Fairbrother <adam.fairbrother@gmail.com>
2024-03-19 23:24:30 -0700

    Added lychee slicer

--- Embiggen.nix
+++ Embiggen.nix
@@ -34,6 +34,7 @@
     extraGroups = [ "networkmanager" "wheel" "video" "docker" ];
     packages = with pkgs; [
       (pkgs.callPackage ./cura5_4-Appimage.nix {} )
+      (pkgs.callPackage ./lychee5_4_3-Appimage.nix {} )
       chromium
       esphome
       firefox
