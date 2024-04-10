d745d45b37877e9f3bfa2b330e20983f40b8b141
Adam Fairbrother <adam.fairbrother@gmail.com>
2024-04-10 11:47:10 -0700

    Added steam to build

--- HePhaestus.nix
+++ HePhaestus.nix
@@ -61,7 +61,15 @@ in
       x2goclient
       xclip
       usbimager
+      steam
+      nmap
     ];
+  };
+  # Steam settings.
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
   };
 }
 
