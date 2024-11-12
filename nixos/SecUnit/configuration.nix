72d86d9d84e8c12eef9544d2ebb7cd2f181fd6bd
Adam Fairbrother <adam.fairbrother@gmail.com>
2024-11-11 23:21:08 -0800

    Mostly Fleshed out Secunit.

--- nixos/SecUnit/configuration.nix
+++ nixos/SecUnit/configuration.nix
@@ -5,46 +5,44 @@
 { inputs, outputs, lib, config, pkgs, secrets, ... }:
 
 let
-  # Sops secret management
-  sops-nix = builtins.fetchTarball {
-    # url = "https://github.com/Mic92/sops-nix/archive/master.tar.gz";
-    # Pinned to 22-02-2024
-    url = "https://github.com/Mic92/sops-nix/archive/f6b80ab6cd25e57f297fe466ad689d8a77057c11.tar.gz";
-    sha256 = "sha256:158n4gwrjpxkgjivmmnlzsy81sxlirmfxgdxhyck5d1pqrwliwls";
-  }; 
-
   ZMStorage = "/storage/tank"; 
   wifiInterface = "wlp2s0";
+  accessPointIP = "192.168.10.1";
 in
 {
   imports =
     [ # Include the results of the hardware scan.
-      (import "${sops-nix}/modules/sops")
       ./hardware-configuration.nix
       ../server.nix
-      #../dokuwiki.nix
       ../users/adam-blank.nix
-      #./suspend2Hibernate.nix
-      #./unstable.nix
-      #./unstable-keybase.nix
-      #./dokuwiki.nix
     ];
 
   networking.hostName = "SecUnit"; # Define your hostname.
 
+  networking.firewall.interfaces.ap0.allowedUDPPorts = [
+      67 # Allow DHCP on local wireless
+      123 # Allow NTP
+  ];
+
   # Mount SSD to the ZM storage location
   fileSystems.${ZMStorage} = { 
-    device = "/dev/disk/by-uuid/5cc187b4-b98c-450c-b28f-c8e58bce7da5";
+    device = "/dev/disk/by-uuid/${secrets.zoneminder.disk-uuid}";
   };
 
+  # Enable the zoneminder service
   services.zoneminder = {
-    enable = false;
+    enable = true;
     storageDir = "${ZMStorage}";
-#    port = 80;
     cameras = 3;
     openFirewall = true;
+    database = {
+      createLocally = true;
+      username = "zoneminder";
+      #Manditory username for localdb
+    };
   };
 
+  #Create access point, local access only
   services.create_ap = {
 	enable = true;
 	settings = {
@@ -53,27 +51,50 @@ in
 	  SSID = "${secrets.zoneminder.wifi_name}";
 	  PASSPHRASE = "${secrets.zoneminder.wifi_pass}";
       FREQ_BAND="2.4";
+      NO_DNSMASQ=1;
+      GATEWAY="${accessPointIP}";
+      IEEE80211N=1;
+      #ISOLATE_CLIENTS=1;
 	};
   };
 
+  #Provide NTP Services
+  services.chrony = {
+    enable = true;
+    extraConfig = ''
+      allow 192.168.10.0/24
+    '';
+  };
+
+  #Provide DNS and DHCP
   services.dnsmasq = {
-    enable = false;
+    enable = true;
     settings = {
       domain-needed = true;
       bogus-priv = true;
       no-resolv = true;
       cache-size = 1000;
-      dhcp-range = [ "${wifiInterface},192.168.10.50,192.168.10.254,24h" ];
-      interface = "${wifiInterface}";
-      dhcp-host = "192.168.10.1";
-      local = "/cam/";
-      domain = "cam";
-      expand-hosts = true;
+      server = [ "1.1.1.1" "8.8.8.8" ];
+      bind-dynamic = true;
 
+      # DHCP Settings
+      interface = "ap0";
       no-hosts = true;
-      address = "/zm.cam/192.168.10.1"; 
+
+      listen-address ="${accessPointIP}";
+      dhcp-option-force = [ 
+        "option:router,${accessPointIP}"
+        "option:dns-server,${accessPointIP}"
+        "option:ntp-server,${accessPointIP}"
+      ];
+      dhcp-range = [ "192.168.10.50,192.168.10.54,24h" ];
+      dhcp-host = [
+        "${secrets.zoneminder.host0.mac},${secrets.zoneminder.host0.name},192.168.10.50" 
+        "${secrets.zoneminder.host1.mac},${secrets.zoneminder.host1.name},192.168.10.51" 
+        "${secrets.zoneminder.host2.mac},${secrets.zoneminder.host2.name},192.168.10.52" 
+        "${secrets.zoneminder.host3.mac},${secrets.zoneminder.host3.name},192.168.10.53" 
+        "${secrets.zoneminder.host4.mac},${secrets.zoneminder.host4.name},192.168.10.54" 
+      ];
     };
   };
-
-
 }
