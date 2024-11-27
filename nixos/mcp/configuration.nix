5c51aeb051d34347db84ac903911ba5bd5b99e2e
Adam Fairbrother <adam.fairbrother@gmail.com>
2024-11-27 10:25:21 -0800

    Added initial config for MCP

---
+++ nixos/mcp/configuration.nix
@@ -0,0 +1,34 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ inputs, outputs, lib, config, pkgs, secrets, ... }:
+
+let
+  hostName      = "mcp";
+  Storage       = "/storage/tank"; 
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+      ../server.nix
+      ../users/adam-blank.nix
+    ];
+
+  hardware.cpu.intel.updateMicrocode = true;
+
+  # Enable harware acceleration for video streams
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-vaapi-driver
+    ];
+  };
+  hardware.intel-gpu-tools.enable = true;
+
+  networking = { 
+    hostName = "${hostName}";
+  };
+
+}
