e014376f245b4b8383007519f72990acb86a562b
Adam Fairbrother <adam.fairbrother@gmail.com>
2025-03-05 14:24:34 -0800

    Updated home assistant configuration

--- nixos/containers/homeAssistant.nix
+++ nixos/containers/homeAssistant.nix
@@ -11,13 +11,9 @@ in
     # Filesystem mount points
     bindMounts = {                                         
       "/var/lib/hass" = {                               
-        hostPath = "/home/container/${hostname}/hass";
+        hostPath = "/home/container/${hostname}";
         isReadOnly = false;                                
       };                                                   
-      "/etc/home-assistant" = {                               
-        hostPath = "/home/container/${hostname}/etc";
-        isReadOnly = false;                                
-      }; 
     };
 
     config = {config, pkgs, lib, ... }: {          
@@ -29,8 +25,7 @@ in
         networkmanager.ethernet.macAddress = "${secrets.${serverName}.containers.${hostname}.mac}";
         firewall = {                                                                                                  
           enable = false;                                   
-          allowedTCPPorts = [ 3000 ];
-          allowedUDPPorts = [ 53 ];
+          allowedTCPPorts = [ 8123 ];
         };                           
         # Use systemd-resolved inside the container 
         # Workaround for bug https://github.com/NixOS/nixpkgs/issues/162686
