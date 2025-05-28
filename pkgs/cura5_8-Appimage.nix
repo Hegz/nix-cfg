301da3e82cf72c97b0c780f9a4e6db4ab4a02d86
Adam Fairbrother <adam.fairbrother@gmail.com>
2025-05-28 10:41:31 -0700

    Added desktop shortcut

--- pkgs/cura5_8-Appimage.nix
+++ pkgs/cura5_8-Appimage.nix
@@ -1,13 +1,26 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ appimageTools, fetchurl }:
 
-with pkgs;
-
-appimageTools.wrapType2 { # or wrapType1
+let
   pname = "cura";
   version = "5.8.1";
   src = fetchurl {
     url = "https://github.com/Ultimaker/Cura/releases/download/5.8.1/UltiMaker-Cura-5.8.1-linux-X64.AppImage";
     hash = "sha256-VLd+V00LhRZYplZbKkEp4DXsqAhA9WLQhF933QAZRX0=";
   };
-  extraPkgs = pkgs: with pkgs; [ ];
+
+  appimageContents = appimageTools.extract {
+    inherit pname version src;
+  };
+in
+  appimageTools.wrapType2 {
+    inherit pname version src;
+
+    # Install a desktop file and icon    
+    extraInstallCommands = ''
+      install -m 444 -D ${appimageContents}/com.ultimaker.cura.desktop $out/share/applications/com.ultimaker.cura.desktop
+      install -m 444 -D ${appimageContents}/cura-icon.png $out/share/icons/hicolor/256x256/apps/cura-icon.png
+      substituteInPlace $out/share/applications/com.ultimaker.cura.desktop \
+            --replace-fail 'Exec=UltiMaker-Cura' 'Exec=${pname}'
+    '';
+
 }
