a674ba8f2e879639169832fb7afb6712a8a9896b
Adam Fairbrother <adam.fairbrother@gmail.com>
2025-06-17 10:45:17 -0700

    Updated firefox settings, Lost TST...

--- home-manager/firefox.nix
+++ home-manager/firefox.nix
@@ -2,7 +2,7 @@
 # Vim configuration options
 {
   nixpkgs.overlays = [
-    inputs.nur.overlay
+    inputs.nur.overlays.default
   ];
   
   programs.firefox = {
@@ -18,56 +18,59 @@
       # user_pref("extensions.autoDisableScopes", 0);
       #'';
       # https://nur.nix-community.org/repos/rycee/
-      extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      extensions.packages = with pkgs.nur.repos.rycee.firefox-addons; [
         bitwarden
         ublock-origin
         privacy-badger
         darkreader
-        tree-style-tab
+#        tree-style-tab
       ];
       settings = {
         "extensions.autoDisableScopes" = 0;
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
         "signon.rememberSignons" = false;
       };
-      bookmarks = [
-        {
-          name = "SD73 Sites";
-          toolbar = true;
-          bookmarks = [
-            {
-              name = "My wiki";
-              tags = [ "wiki" ];
-              keyword = "wiki";
-              url = "http://cromulent/";
-            }
-            {
-              name = "SD73 Email";
-              url = "https://outlook.office.com/mail/inbox/";
-            }
-            {
-              name = "Teams";
-              url = "https://teams.microsoft.com/v2/";
-            }
-            {
-              name = "GLPI";
-              url = "https://assets.sd73.bc.ca";
-            }
-            {
-              name = "Oasis";
-              url = "srb-web.sd73.bc.ca";
-            }
-            {
-              name = "Q";
-              url = "https://q.sd73.bc.ca/index.php";
-            }
-            {
-              name = "SD73 Outages";
-              url = "https://www.sd73.bc.ca/en/working-together-departments/it-inter.aspx";
-            }
-          ];
-        }
-      ];
+      bookmarks = {
+        force = true;
+        settings = [
+          {
+            name = "SD73 Sites";
+            toolbar = true;
+            bookmarks = [
+              {
+                name = "My wiki";
+                tags = [ "wiki" ];
+                keyword = "wiki";
+                url = "http://cromulent/";
+              }
+              {
+                name = "SD73 Email";
+                url = "https://outlook.office.com/mail/inbox/";
+              }
+              {
+                name = "Teams";
+                url = "https://teams.microsoft.com/v2/";
+              }
+              {
+                name = "GLPI";
+                url = "https://assets.sd73.bc.ca";
+              }
+              {
+                name = "Oasis";
+                url = "srb-web.sd73.bc.ca";
+              }
+              {
+                name = "Q";
+                url = "https://q.sd73.bc.ca/index.php";
+              }
+              {
+                name = "SD73 Outages";
+                url = "https://www.sd73.bc.ca/en/working-together-departments/it-inter.aspx";
+              }
+            ];
+          }
+        ];
+      };
     };
   };
 }
