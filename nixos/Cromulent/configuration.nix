de658d765ba3cad4d336f5e073f18ca943218efd
Adam Fairbrother <adam.fairbrother@gmail.com>
2025-12-01 13:36:57 -0800

    Updated AMD configuration for 25.11

--- nixos/Cromulent/configuration.nix
+++ nixos/Cromulent/configuration.nix
@@ -47,14 +47,6 @@ in
   boot.initrd.kernelModules = [ "amdgpu" ];
   hardware.graphics.enable32Bit = true; # For 32 bit applications
 
-  hardware.graphics.extraPackages = with pkgs; [
-      amdvlk
-    ];
-    # For 32 bit applications 
-  hardware.graphics.extraPackages32 = with pkgs; [
-      driversi686Linux.amdvlk
-    ];
-
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "amdgpu" ];  
 
