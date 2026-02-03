ffb765edcb62f3ecb53e0de87f79ad60d3e18775
Adam Fairbrother <adam.fairbrother@gmail.com>
2026-02-02 22:56:35 -0800

    Overlays! such a problem solver.

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,21 +7,36 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
-
-#      orca-slicer = prev.orca-slicer.overrideAttrs (old: {
-#        postInstall = (old.postInstall or "") + ''
-#          mv $out/bin/orca-slicer $out/bin/.orca-slicer-wrapped
-#          echo "env __GLX_VENDOR_LIBRARY_NAME=mesa __EGL_VENDOR_LIBRARY_FILENAMES=/run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json MESA_LOADER_DRIVER_OVERRIDE=zink GALLIUM_DRIVER=zink WEBKIT_DISABLE_DMABUF_RENDERER=1 $out/bin/.orca-slicer-wrapped" > $out/bin/orca-slicer
-#          chmod +x $out/bin/orca-slicer
-#        '';
-#      });
+    orca-slicer = final.symlinkJoin {
+      # Create a wrapper for orca slicer that includes the environment allowing it to work.
+      name = "orca-slicer";
+      paths = [ prev.orca-slicer ];
+      buildInputs = [ final.makeWrapper ];
+      postBuild = ''
+          wrapProgram $out/bin/orca-slicer \
+            --set __GLX_VENDOR_LIBRARY_NAME mesa \
+            --set __EGL_VENDOR_LIBRARY_FILENAMES /run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json \
+            --set MESA_LOADER_DRIVER_OVERRIDE zink \
+            --set GALLIUM_DRIVER zink \
+            --set WEBKIT_DISABLE_DMABUF_RENDERER 1
+        '';
+      };
+    freecad = final.symlinkJoin {
+      # Create a wrapper for freecad that includes the environment allowing it to work.
+      name = "freecad";
+      paths = [ prev.freecad ];
+      buildInputs = [ final.makeWrapper ];
+      postBuild = ''
+          wrapProgram $out/bin/freecad \
+            --set __GLX_VENDOR_LIBRARY_NAME mesa \
+            --set __EGL_VENDOR_LIBRARY_FILENAMES /run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json \
+            --set MESA_LOADER_DRIVER_OVERRIDE zink \
+            --set GALLIUM_DRIVER zink \
+            --set WEBKIT_DISABLE_DMABUF_RENDERER 1
+        '';
+      };
   };
 
-  # When applied, the unstable nixpkgs set (declared in the flake inputs) will
-  # be accessible through 'pkgs.unstable'
   unstable-packages = final: _prev: {
     unstable = import inputs.nixpkgs-unstable {
       system = final.system;
