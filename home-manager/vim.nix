7cce5d48aa9aefb18925235c2d29030155a58a71
Adam Fairbrother <adam.fairbrother@gmail.com>
2026-05-15 11:11:09 -0700

    Fixed vim llm code completion

--- home-manager/vim.nix
+++ home-manager/vim.nix
@@ -23,28 +23,27 @@
       let g:syntastic_check_on_open = 1
       let g:syntastic_check_on_wq = 0
 
-	  " llama.vim - initialise after plugins are loaded
-	  function! SetupLlama()
-		if exists('g:llama_config') && type(g:llama_config) == v:t_dict
-		  " already initialised by plugin, just set our values
-		else
-		  let g:llama_config = {}
-		endif
-        let g:llama_config.endpoint_fim  = 'http://${secrets.llama.host}:${secrets.llama.port}/infill'
-        let g:llama_config.endpoint_inst = 'http://${secrets.llama.host}:${secrets.llama.port}/completion' 
-		let g:llama_config.model         = 'fim-coder'
-		let g:llama_config.n_prefix      = 256
-	  	let g:llama_config.n_suffix      = 64
-		let g:llama_config.n_predict     = 128
-		let g:llama_config.auto_fim      = v:true
-		let g:llama_config.show_info     = 1
-		let g:llama_config.t_max_prompt_ms  = 500
-		let g:llama_config.t_max_predict_ms = 3000
-		let g:llama_config.log_level     = 'debug'
-		let g:llama_config.log_file      = '/tmp/llama_vim.log'
-	  endfunction
+      " llama.vim - initialise after plugins are loaded
+      function! SetupLlama()
+        if exists('g:llama_config') && type(g:llama_config) == v:t_dict
+          " already initialised by plugin, just set our values
+        else
+          let g:llama_config = {}
+        endif
+        let g:llama_config.endpoint  = 'http://${secrets.llama.host}:${secrets.llama.port}/infill'
+        let g:llama_config.model         = 'fim-coder'
+        let g:llama_config.n_prefix      = 256
+        let g:llama_config.n_suffix      = 64
+        let g:llama_config.n_predict     = 128
+        let g:llama_config.auto_fim      = v:true
+        let g:llama_config.show_info     = 1
+        let g:llama_config.t_max_prompt_ms  = 500
+        let g:llama_config.t_max_predict_ms = 3000
+        let g:llama_config.log_level     = 'debug'
+        let g:llama_config.log_file      = '/tmp/llama_vim.log'
+      endfunction
 
-	  autocmd VimEnter * call SetupLlama()
+      autocmd VimEnter * call SetupLlama()
     '';
     plugins = with pkgs.vimPlugins; [ 
       #cmp-copilot 
