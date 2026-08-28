00391fc6adb2e15872da08f5f6af6e0eb3df047d
Adam Fairbrother <adam.fairbrother@gmail.com>
2026-08-28 12:41:42 -0700

    formating

--- home-manager/adam.nix
+++ home-manager/adam.nix
@@ -1,19 +1,25 @@
-{ inputs, outputs, lib, config, pkgs, ... }:
 {
+  inputs,
+  outputs,
+  lib,
+  config,
+  pkgs,
+  ...
+}: {
   # You can import other home-manager modules here
   imports = [
-      ./git.nix
-      ./ssh.nix
-      ./tmux.nix
-      ./vim.nix
-      ./zsh.nix
-      ./home.nix
-      ./opencode.nix
+    ./git.nix
+    ./ssh.nix
+    ./tmux.nix
+    ./vim.nix
+    ./zsh.nix
+    ./home.nix
+    ./opencode.nix
+    ./firefox.nix
   ];
 
   home = {
     username = "adam";
     homeDirectory = "/home/adam";
   };
-
 }
